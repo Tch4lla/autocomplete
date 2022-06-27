b86aeb577886e741b34433648b57528862a246e4
Sulaymane Sillah <sssillah1@gmail.com>
2022-06-28 06:36:16 +0900

    autocomplete search for movies

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {MongoClient, ObjectID, ObjectId} = require('mongodb')
+require('dotenv').config()
+const PORT = 4000
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
+
+MongoClient.connect(dbConnectionStr)
+    .then(client =>{
+        console.log('Connected to database')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
+//Bit of middleware to help parse data
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+//request methods, allows server to talk back and forth between client 
+//first get request reads data from server, brings back array of possibilities
+//brings back data while I'm typing
+app.get('/search', async(request,response) => {
+    try {
+        //sending search object to mongodb to have mongo search the db
+        let result = await collection.aggregate([
+            {
+                '$search' : {
+                    'autocomplete' : {
+                        'query': `${request.query.query}`,
+                        'path':'title',
+                        'fuzzy': {
+                            'maxedits':2,
+                            'prefixlength':3
+                        }
+                    }
+                }
+            }
+        ]).toArray()
+        response.send(result)
+    } catch(error){
+        response.status(500).send({message: error.message})
+
+    }
+})
+
+//get request when the user actually selects an item, bring back the information from the selection
+app.get('/get/:id',async (request,response)=> {
+    try{
+        let result = await collection.findOne({
+            //sending the object id to mongo for the document that I want to find 
+            //object id is the same as the id parameter that I'm passing in
+            '_id': ObjectId(request.params.id)
+        })
+        //send result back if the async function was fulfilled 
+        response.send(result)
+    }catch(error){
+        response.status(500).send({message: error.message})
+
+    }
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log('Sever is runnuing')
+})
